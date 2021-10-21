6c818831d5e1ab80fe82158a8eccd288e0e61d9f
김범진 <sdpthf@naver.com>
2021-10-21 12:05:45 +0900

    Init Project

---
+++ src/main/java/com/dsm/nms/NmsBackendV1Application.java
@@ -0,0 +1,13 @@
+package com.dsm.nms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmsBackendV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmsBackendV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsm/nms/NmsBackendV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.nms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmsBackendV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
