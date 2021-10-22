958560ca0321cf008fa2b9770f705b14933c9dc3
김범진 <sdpthf@naver.com>
2021-10-22 23:56:25 +0900

    #11 [ADD] 공지사항 엔티티

---
+++ src/main/java/com/dsm/nms/entity/notice/Notice.java
@@ -0,0 +1,53 @@
+package com.dsm.nms.entity.notice;
+
+import com.dsm.nms.entity.BaseTimeEntity;
+import com.dsm.nms.entity.image.Image;
+import com.dsm.nms.entity.notice.target.Target;
+import com.dsm.nms.entity.star.Star;
+import com.dsm.nms.entity.teacher.Teacher;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notice extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToMany(mappedBy = "notice")
+    private Set<Target> targets = new HashSet<>();
+
+    @OneToMany(mappedBy = "notice", cascade = CascadeType.REMOVE)
+    private List<Image> images;
+
+    @OneToMany(mappedBy = "notice", cascade = CascadeType.REMOVE)
+    private Set<Star> stars = new HashSet<>();
+
+    @JoinColumn(name = "teacher_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Teacher teacher;
+
+    @Builder
+    public Notice(String title, String content, Set<Target> targets, List<Image> images, Teacher teacher) {
+        this.title = title;
+        this.content = content;
+        this.targets = targets;
+        this.images = images;
+        this.teacher = teacher;
+    }
+
+}
