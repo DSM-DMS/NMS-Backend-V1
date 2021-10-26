37937e06c263f406c7c1eaefdd625529d2120c16
김범진 <sdpthf@naver.com>
2021-10-26 20:01:49 +0900

    #28 [UPDATE] 사진 순서

--- src/main/java/com/dsm/nms/entity/image/Image.java
+++ src/main/java/com/dsm/nms/entity/image/Image.java
@@ -15,6 +15,7 @@ public class Image {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    private Integer order = 1;
     private String imagePath;
     private String imageUrl;
 
