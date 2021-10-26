c2c5581c8297c7a9ead1dc5ddb5996f85af84d62
김범진 <sdpthf@naver.com>
2021-10-26 10:36:48 +0900

    #21 [ADD] 학생 리포지토리

---
+++ src/main/java/com/dsm/nms/entity/student/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.dsm.nms.entity.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
