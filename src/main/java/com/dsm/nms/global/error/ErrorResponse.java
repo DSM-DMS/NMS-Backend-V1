ba02bdc5886f646bf82fe808d004c41a28c12a4e
김범진 <sdpthf@naver.com>
2021-11-11 11:56:12 +0900

    📑 :: ErrorResponse

---
+++ src/main/java/com/dsm/nms/global/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.dsm.nms.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
