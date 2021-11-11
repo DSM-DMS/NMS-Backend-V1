1ac71464f8f55b7e9760135273a2ac33b677e789
김범진 <sdpthf@naver.com>
2021-11-11 11:37:46 +0900

    ⚡️ :: findByEmail

--- src/main/java/com/dsm/nms/domain/student/StudentRepository.java
+++ src/main/java/com/dsm/nms/domain/student/StudentRepository.java
@@ -1,6 +1,9 @@
-package com.dsm.nms.entity.student;
+package com.dsm.nms.domain.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Optional<Student> findByEmail(String email);
 }
