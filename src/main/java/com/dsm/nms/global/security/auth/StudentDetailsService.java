3f26182fdb4655ff686482149f6dafb858c338e1
김범진 <sdpthf@naver.com>
2021-11-11 11:38:00 +0900

    📑 :: StudentDetailsService

---
+++ src/main/java/com/dsm/nms/global/security/auth/StudentDetailsService.java
@@ -0,0 +1,23 @@
+package com.dsm.nms.global.security.auth;
+
+import com.dsm.nms.domain.student.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StudentDetailsService implements UserDetailsService {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new StudentDetails(
+                studentRepository.findByEmail(email)
+                        .orElseThrow());
+    }
+
+}
