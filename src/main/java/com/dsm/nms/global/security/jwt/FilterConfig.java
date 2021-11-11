e50e806d39e4a5029397093ec275d5213b54e859
김범진 <sdpthf@naver.com>
2021-11-11 19:00:46 +0900

    📑 :: FilterConfig

---
+++ src/main/java/com/dsm/nms/global/security/jwt/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.dsm.nms.global.security.jwt;
+
+import com.dsm.nms.global.error.ExceptionFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public void configure(HttpSecurity security) {
+        JwtTokenFilter jwtTokenFilter = new JwtTokenFilter(jwtTokenProvider);
+        ExceptionFilter exceptionFilter = new ExceptionFilter();
+
+        security.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        security.addFilterBefore(exceptionFilter, JwtTokenFilter.class);
+    }
+
+}
