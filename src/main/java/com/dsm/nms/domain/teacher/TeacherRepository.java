65add5675fdb55982a06cd5e0671801697f01575
김범진 <sdpthf@naver.com>
2021-11-11 11:37:40 +0900

    ⚡️ :: findByUsername

--- src/main/java/com/dsm/nms/domain/teacher/TeacherRepository.java
+++ src/main/java/com/dsm/nms/domain/teacher/TeacherRepository.java
@@ -1,6 +1,9 @@
-package com.dsm.nms.entity.teacher;
+package com.dsm.nms.domain.teacher;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+    Optional<Teacher> findByUsername(String username);
 }
