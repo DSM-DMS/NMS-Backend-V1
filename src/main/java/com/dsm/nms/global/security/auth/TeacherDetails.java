ba7e2e0cc3289cbf8c91bca672e3a0d928b9b5c2
김범진 <sdpthf@naver.com>
2021-11-11 11:37:11 +0900

    📑 :: TeacherDetails

---
+++ src/main/java/com/dsm/nms/global/security/auth/TeacherDetails.java
@@ -0,0 +1,53 @@
+package com.dsm.nms.global.security.auth;
+
+import com.dsm.nms.domain.teacher.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@AllArgsConstructor
+public class TeacherDetails implements UserDetails {
+
+    private final Teacher teacher;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return teacher.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return teacher.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
