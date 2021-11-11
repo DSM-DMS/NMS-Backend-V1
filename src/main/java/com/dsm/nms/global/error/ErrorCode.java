777f983264034e7e302377dcca5f7232885443c5
김범진 <sdpthf@naver.com>
2021-11-11 20:22:41 +0900

    :bookmark_tabs: :: InvalidRoleException

--- src/main/java/com/dsm/nms/global/error/ErrorCode.java
+++ src/main/java/com/dsm/nms/global/error/ErrorCode.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INVALID_TOKEN(401, "Invalid Token"),
-    EXPIRED_TOKEN(401, "Expired Token");
+    EXPIRED_TOKEN(401, "Expired Token"),
+
+    INVALID_ROLE(401, "Invalid Role")
+    ;
 
     private final int status;
     private final String message;
