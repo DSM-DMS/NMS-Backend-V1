a31d2038edd0c8fd7a9d5ae770a8a410e2341b23
이준서 <ljchan71@gmail.com>
2021-11-11 17:09:16 +0900

    📑 :: JwtProperties

---
+++ src/main/java/com/dsm/nms/global/security/jwt/JwtProperties.java
@@ -0,0 +1,20 @@
+package com.dsm.nms.global.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+
+    private final String secretKey;
+    private final Long accessExp;
+    private final Long refreshExp;
+    private final String header;
+    private final String prefix;
+
+}
