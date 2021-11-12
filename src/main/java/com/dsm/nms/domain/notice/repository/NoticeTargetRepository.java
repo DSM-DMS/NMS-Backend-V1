a76275f53fcfe1306ce4cf0b92140fa5b3f38009
이준서 <ljchan71@gmail.com>
2021-11-12 18:54:35 +0900

    🐛 :: import 빼먹은거

--- src/main/java/com/dsm/nms/domain/notice/repository/NoticeTargetRepository.java
+++ src/main/java/com/dsm/nms/domain/notice/repository/NoticeTargetRepository.java
@@ -1,5 +1,7 @@
 package com.dsm.nms.domain.notice.repository;
 
+import com.dsm.nms.domain.notice.entity.noticetarget.NoticeTarget;
+import com.dsm.nms.domain.notice.entity.noticetarget.NoticeTargetId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeTargetRepository extends JpaRepository<NoticeTarget, NoticeTargetId> {
--- src/main/java/com/dsm/nms/domain/notice/repository/TargetRepository.java
+++ src/main/java/com/dsm/nms/domain/notice/repository/TargetRepository.java
@@ -1,5 +1,6 @@
 package com.dsm.nms.domain.notice.repository;
 
+import com.dsm.nms.domain.notice.entity.target.Target;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TargetRepository extends JpaRepository<Target, Integer> {
