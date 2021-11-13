d1238f0c810555ea564cee74c47d0073464278d1
김범진 <sdpthf@naver.com>
2021-11-13 19:14:08 +0900

    📑 :: ImageService

---
+++ src/main/java/com/dsm/nms/domain/image/service/ImageServiceImpl.java
@@ -0,0 +1,9 @@
+package com.dsm.nms.domain.image.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ImageServiceImpl implements ImageService {
+}
