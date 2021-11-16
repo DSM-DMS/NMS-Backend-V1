5a47aba145da7dfed198a4374d847df86bcf5fa5
이준서 <ljchan71@gmail.com>
2021-11-16 14:41:13 +0900

    🐛 :: 이거 누가했어!

--- src/main/java/com/dsm/nms/domain/teacher/api/TeacherController.java
+++ src/main/java/com/dsm/nms/domain/teacher/api/TeacherController.java
@@ -1,13 +1,10 @@
 package com.dsm.nms.domain.teacher.api;
 
-import com.dsm.nms.domain.teacher.service.TeacherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class TeacherController {
-
-    private final TeacherService teacherService;
 
 }
