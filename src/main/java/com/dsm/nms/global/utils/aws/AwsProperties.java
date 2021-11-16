baf4fc322740fdd941552ce1ab0926780e214ab0
이준서 <ljchan71@gmail.com>
2021-11-16 14:49:51 +0900

    📑 :: AwsProperties

---
+++ src/main/java/com/dsm/nms/global/utils/aws/AwsProperties.java
@@ -0,0 +1,17 @@
+package com.dsm.nms.global.utils.aws;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "aws")
+public class AwsProperties {
+
+    private final String accessKey;
+    private final String secretKey;
+    private final String region;
+}
