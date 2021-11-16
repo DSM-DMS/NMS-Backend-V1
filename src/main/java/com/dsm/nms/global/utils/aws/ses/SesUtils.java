cc202720729e9fb14983a71c8ec36cc96edcd18b
이준서 <ljchan71@gmail.com>
2021-11-16 14:50:04 +0900

    📑 :: SesUtils

---
+++ src/main/java/com/dsm/nms/global/utils/aws/ses/SesUtils.java
@@ -0,0 +1,32 @@
+package com.dsm.nms.global.utils.aws.ses;
+
+import com.amazonaws.services.simpleemail.model.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class SesUtils {
+
+    private final SesConfig sesConfig;
+
+    public void sendEmail(String from, String to, String authCode) {
+        Message message = new Message()
+                .withSubject(createContent("NMS 이메일 인증"))
+                .withBody(new Body()
+                        .withHtml(createContent(authCode)));
+
+        SendEmailRequest sendEmailRequest = new SendEmailRequest()
+                .withSource(from)
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(message);
+
+        sesConfig.amazonSimpleEmailService().sendEmail(sendEmailRequest);
+    }
+
+    private Content createContent(String text) {
+        return new Content()
+                .withCharset("UTF-8")
+                .withData(text);
+    }
+}
