0dfe15a6327c7a065c56b7ecaf449abe66443a2f
이준서 <ljchan71@gmail.com>
2021-11-17 08:25:11 +0900

    ♻️ :: from 이메일 환경변수화

--- src/main/java/com/dsm/nms/global/utils/aws/ses/SesUtils.java
+++ src/main/java/com/dsm/nms/global/utils/aws/ses/SesUtils.java
@@ -2,15 +2,19 @@ package com.dsm.nms.global.utils.aws.ses;
 
 import com.amazonaws.services.simpleemail.model.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class SesUtils {
 
+    @Value("${aws.ses.baseEmail}")
+    private String from;
+
     private final SesConfig sesConfig;
 
-    public void sendEmail(String from, String to, String authCode) {
+    public void sendEmail(String to, String authCode) {
         Message message = new Message()
                 .withSubject(createContent("NMS 이메일 인증"))
                 .withBody(new Body()
