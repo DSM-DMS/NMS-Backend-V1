6bd03a9c0087ada1322d7682aa6a9a17c15b2a9e
이준서 <ljchan71@gmail.com>
2021-11-18 12:24:28 +0900

    ♻️ :: 이름을 바꿔요

--- src/main/java/com/dsm/nms/domain/auth/service/AuthCodeService.java
+++ src/main/java/com/dsm/nms/domain/auth/service/AuthCodeService.java
@@ -1,9 +1,9 @@
 package com.dsm.nms.domain.auth.service;
 
 import com.dsm.nms.domain.auth.api.dto.request.SendCodeRequest;
-import com.dsm.nms.domain.auth.api.dto.request.VerifyCodeRequest;
+import com.dsm.nms.domain.auth.api.dto.request.CertifyCodeRequest;
 
 public interface AuthCodeService {
     void sendCode(SendCodeRequest sendCodeRequest);
-    void verifyCode(VerifyCodeRequest verifyCodeRequest);
+    void certifyCode(CertifyCodeRequest CertifyCodeRequest);
 }
