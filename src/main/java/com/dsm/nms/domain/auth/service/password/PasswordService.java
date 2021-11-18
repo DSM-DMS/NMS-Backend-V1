a901b526aea072da5aa13da356dba060d340eac3
김범진 <sdpthf@naver.com>
2021-11-19 08:37:49 +0900

    ⚡️ :: changePassword

--- src/main/java/com/dsm/nms/domain/auth/service/password/PasswordService.java
+++ src/main/java/com/dsm/nms/domain/auth/service/password/PasswordService.java
@@ -3,5 +3,6 @@ package com.dsm.nms.domain.auth.service.password;
 import com.dsm.nms.domain.auth.api.dto.request.PasswordRequest;
 
 public interface PasswordService {
-    public void verifyPassword(PasswordRequest passwordRequest);
+    void verifyPassword(PasswordRequest passwordRequest);
+    void changePassword(PasswordRequest passwordRequest);
 }
