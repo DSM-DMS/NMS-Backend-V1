88f08801fd64c02ffb26c76a5d448076df90be58
김범진 <sdpthf@naver.com>
2021-11-18 12:07:56 +0900

    ⚡️ :: 이메일과 비밀번호 불러오기

--- src/main/java/com/dsm/nms/domain/teacher/facade/TeacherFacade.java
+++ src/main/java/com/dsm/nms/domain/teacher/facade/TeacherFacade.java
@@ -1,14 +1,18 @@
 package com.dsm.nms.domain.teacher.facade;
 
+import com.dsm.nms.domain.teacher.entity.Teacher;
 import com.dsm.nms.domain.teacher.exception.TeacherAlreadyExistsException;
+import com.dsm.nms.domain.teacher.exception.TeacherNotFouncException;
 import com.dsm.nms.domain.teacher.repository.TeacherRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class TeacherFacade {
 
+    private final PasswordEncoder passwordEncoder;
     private final TeacherRepository teacherRepository;
 
     public boolean isAlreadyExists(String email) {
@@ -17,4 +21,14 @@ public class TeacherFacade {
         }
         return true;
     }
+
+    public Teacher getByEmail(String email) {
+        return teacherRepository.findByEmail(email)
+                .orElseThrow(() -> TeacherNotFouncException.EXCEPTION);
+    }
+
+    public String getPasswordByEmail(String email) {
+        return passwordEncoder.encode(getByEmail(email).getPassword());
+    }
+
 }
