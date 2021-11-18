3a585ec00f8e466798bf75efbcf9e80b28e8f02f
김범진 <sdpthf@naver.com>
2021-11-18 16:23:50 +0900

    ⚡️ :: 비밀번호 패턴 변경

--- src/main/java/com/dsm/nms/domain/auth/api/dto/request/PasswordRequest.java
+++ src/main/java/com/dsm/nms/domain/auth/api/dto/request/PasswordRequest.java
@@ -16,7 +16,7 @@ public class PasswordRequest {
     private String email;
 
     @NotBlank(message = "password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,-./:;<=>?@＼^_`{|}~]){8,32}$",
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[!#$%&'()*+,-./:;<=>?@＼^_`{|}~])(?=.*[0-9])(?=\\S+$).{8,32}$",
             message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다.")
     private String password;
 
