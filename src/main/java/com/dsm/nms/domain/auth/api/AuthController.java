03da7aa77b15cd2a4be70a4456d4b68d3771d0cf
김범진 <sdpthf@naver.com>
2021-11-18 12:06:25 +0900

    ⚡️ :: verifyPasswordController

--- src/main/java/com/dsm/nms/domain/auth/api/AuthController.java
+++ src/main/java/com/dsm/nms/domain/auth/api/AuthController.java
@@ -2,26 +2,42 @@ package com.dsm.nms.domain.auth.api;
 
 import com.dsm.nms.domain.auth.api.dto.request.SendCodeRequest;
 import com.dsm.nms.domain.auth.api.dto.request.VerifyCodeRequest;
+import com.dsm.nms.domain.auth.api.dto.request.PasswordRequest;
 import com.dsm.nms.domain.auth.service.AuthCodeService;
+import com.dsm.nms.domain.auth.service.password.PasswordService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/email")
+@RequestMapping
 public class AuthController {
 
     private final AuthCodeService authCodeService;
+    private final PasswordService passwordService;
 
-    @PostMapping("/")
+    @PostMapping("/email")
     public void sendCode(@RequestBody @Valid SendCodeRequest sendCodeRequest) {
         authCodeService.sendCode(sendCodeRequest);
     }
 
-    @PutMapping("/")
+    @PutMapping("/email")
     public void verifyCode(@RequestBody @Valid VerifyCodeRequest verifyCodeRequest) {
         authCodeService.verifyCode(verifyCodeRequest);
     }
+
+    @PostMapping("/student/password")
+    public void verifyStudentPassword(@RequestBody @Valid PasswordRequest passwordRequest) {
+        passwordService.verifyPassword(passwordRequest);
+    }
+
+    @PostMapping("/teacher/password")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void verifyTeacherPassword(@RequestBody @Valid PasswordRequest passwordRequest) {
+        passwordService.verifyPassword(passwordRequest);
+    }
+
 }
