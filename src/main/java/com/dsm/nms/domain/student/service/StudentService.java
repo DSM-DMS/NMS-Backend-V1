506c1397426709df87e8657e51331c382c6877a7
이준서 <ljchan71@gmail.com>
2021-11-22 22:42:42 +0900

    ⚡️ :: student 로그인 로직

--- src/main/java/com/dsm/nms/domain/student/service/StudentService.java
+++ src/main/java/com/dsm/nms/domain/student/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.dsm.nms.domain.student.service;
 
-import com.dsm.nms.domain.student.api.dto.request.SignUpRequest;
+import com.dsm.nms.global.utils.auth.dto.request.LoginRequest;
+import com.dsm.nms.global.utils.auth.dto.request.StudentSignUpRequest;
+import com.dsm.nms.global.utils.auth.dto.response.TokenResponse;
 
 public interface StudentService {
-    void singUp(SignUpRequest signUpRequest);
+    void singUp(StudentSignUpRequest studentSignUpRequest);
+    TokenResponse login(LoginRequest loginRequest);
 }
--- src/main/java/com/dsm/nms/domain/student/service/StudentServiceImpl.java
+++ src/main/java/com/dsm/nms/domain/student/service/StudentServiceImpl.java
@@ -1,12 +1,19 @@
 package com.dsm.nms.domain.student.service;
 
-import com.dsm.nms.domain.student.api.dto.request.SignUpRequest;
 import com.dsm.nms.domain.student.entity.Student;
 import com.dsm.nms.domain.student.facade.StudentFacade;
 import com.dsm.nms.domain.student.repository.StudentRepository;
 import com.dsm.nms.global.utils.auth.user.UserUtil;
+import com.dsm.nms.global.exception.InvalidPasswordException;
+import com.dsm.nms.global.security.jwt.JwtTokenProvider;
+import com.dsm.nms.global.utils.auth.dto.request.LoginRequest;
+import com.dsm.nms.global.utils.auth.dto.request.StudentSignUpRequest;
+import com.dsm.nms.global.utils.auth.dto.response.TokenResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,11 +22,26 @@ public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
     private final StudentFacade studentFacade;
     private final UserUtil userUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider tokenProvider;
 
     @Override
-    public void singUp(SignUpRequest signUpRequest) {
-        studentFacade.existNicknameFilter(signUpRequest.getNickname());
-        userUtil.existEmailFilter(signUpRequest.getEmail());
-        studentRepository.save(new Student(signUpRequest));
+    public void singUp(StudentSignUpRequest studentSignUpRequest) {
+        studentFacade.existNicknameFilter(studentSignUpRequest.getNickname());
+        userUtil.existEmailFilter(studentSignUpRequest.getEmail());
+
+        studentRepository.save(new Student(studentSignUpRequest));
+    }
+
+    @Override
+    public TokenResponse login(LoginRequest loginRequest) {
+        return Optional.of(studentFacade.getByEmail(loginRequest.getEmail()))
+                .filter(
+                        student -> passwordEncoder.matches(loginRequest.getPassword(), student.getPassword())
+                )
+                .map(
+                        student -> tokenProvider.generateToken(loginRequest.getEmail(), "student")
+                )
+                .orElseThrow(() -> InvalidPasswordException.EXCEPTION);
     }
 }
