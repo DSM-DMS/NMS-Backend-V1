c63f8fdd610bdfec5bf102e272fe2b0d212acff1
김범진 <sdpthf@naver.com>
2021-11-25 21:50:24 +0900

    ⚡ :: getByNoticeId

--- src/main/java/com/dsm/nms/domain/notice/facade/NoticeFacade.java
+++ src/main/java/com/dsm/nms/domain/notice/facade/NoticeFacade.java
@@ -4,9 +4,12 @@ import com.dsm.nms.domain.notice.entity.Notice;
 import com.dsm.nms.domain.notice.entity.noticetarget.NoticeTarget;
 import com.dsm.nms.domain.notice.entity.target.Target;
 import com.dsm.nms.domain.notice.entity.target.TargetTag;
+import com.dsm.nms.domain.notice.exception.NoticeNotFoundException;
 import com.dsm.nms.domain.notice.exception.TargetNotFoundException;
+import com.dsm.nms.domain.notice.repository.NoticeRepository;
 import com.dsm.nms.domain.notice.repository.NoticeTargetRepository;
 import com.dsm.nms.domain.notice.repository.TargetRepository;
+import com.dsm.nms.global.aop.Enum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,6 +21,7 @@ import java.util.Optional;
 public class NoticeFacade {
 
     private final TargetRepository targetRepository;
+    private final NoticeRepository noticeRepository;
     private final NoticeTargetRepository noticeTargetRepository;
 
     public void addTargetTags(Notice notice, List<TargetTag> tags) {
@@ -25,6 +29,11 @@ public class NoticeFacade {
                 .target(getTarget(targetTag))
                 .notice(notice)
                 .build()));
+    }
+
+    public Notice getByNoticeId(Integer noticeId) {
+        return noticeRepository.findById(noticeId)
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
     }
 
     private Target getTarget(TargetTag tag) {
