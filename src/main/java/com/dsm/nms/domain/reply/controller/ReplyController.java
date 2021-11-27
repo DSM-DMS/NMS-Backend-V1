bd1538aa5c136c8f95a9c057c3f3101b6944bf5a
김범진 <sdpthf@naver.com>
2021-11-27 18:40:28 +0900

    📑 :: ReplyController

---
+++ src/main/java/com/dsm/nms/domain/reply/controller/ReplyController.java
@@ -0,0 +1,21 @@
+package com.dsm.nms.domain.reply.controller;
+
+import com.dsm.nms.domain.reply.service.ReplyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/reply")
+@RestController
+public class ReplyController {
+
+    private final ReplyService replyService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void addReply(@RequestParam Integer commentId, @RequestBody String content) {
+        replyService.addReply(commentId, content);
+    }
+
+}
