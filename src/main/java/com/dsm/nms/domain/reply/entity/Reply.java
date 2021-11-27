73febf22daf94720d4575fc294dc430a3dbed12b
김범진 <sdpthf@naver.com>
2021-11-27 21:45:06 +0900

    ⚡ :: ReplyFacade

--- src/main/java/com/dsm/nms/domain/reply/entity/Reply.java
+++ src/main/java/com/dsm/nms/domain/reply/entity/Reply.java
@@ -1,12 +1,13 @@
 package com.dsm.nms.domain.reply.entity;
 
-import com.dsm.nms.global.entity.BaseTimeEntity;
 import com.dsm.nms.domain.comment.entity.Comment;
+import com.dsm.nms.domain.student.entity.Student;
 import com.dsm.nms.domain.teacher.entity.Teacher;
-import com.dsm.nms.global.entity.Writer;
+import com.dsm.nms.global.entity.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Getter
@@ -24,18 +25,14 @@ public class Reply extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private Comment comment;
 
-    @AttributeOverrides({
-            @AttributeOverride(name = "name", column = @Column(name = "teacher_name")),
-            @AttributeOverride(name = "profileUrl", column = @Column(name = "teacher_profile_url"))
-    })
-    @Embedded
-    private Writer writer;
+    @JoinColumn(name = "teacher_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Teacher teacher;
 
     public Reply(Comment comment, String content, Teacher teacher) {
         this.content = content;
         this.comment = comment;
-        this.writer.setName(teacher.getName());
-        this.writer.setProfileUrl(teacher.getProfileUrl());
+        this.teacher = teacher;
     }
 
 }
