24dd692e4cd02469fdcc0cc17e6657dc35b7fdc6
이준서 <ljchan71@gmail.com>
2021-11-28 04:53:04 +0900

    📑 :: CommentController

---
+++ src/main/java/com/dsm/nms/domain/comment/api/CommentController.java
@@ -0,0 +1,33 @@
+package com.dsm.nms.domain.comment.api;
+
+import com.dsm.nms.domain.comment.api.dto.request.CommentRequest;
+import com.dsm.nms.domain.comment.service.CommentService;
+import com.dsm.nms.global.security.auth.StudentDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+@RestController
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void comment(@RequestParam("notice-id") int noticeId,
+                        @RequestBody @Valid CommentRequest commentRequest) {
+
+        commentService.comment(noticeId, commentRequest);
+    }
+
+    @DeleteMapping("/")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delComment(@RequestParam("comment-id") int commentId) {
+        commentService.
+    }
+}
