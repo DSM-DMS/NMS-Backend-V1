cd6fb033db5553b6832150594752aa47005ca611
김범진 <sdpthf@naver.com>
2021-11-27 13:51:46 +0900

    ⚡ :: comment <-> reply 연관관계

--- src/main/java/com/dsm/nms/domain/comment/entity/Comment.java
+++ src/main/java/com/dsm/nms/domain/comment/entity/Comment.java
@@ -2,12 +2,14 @@ package com.dsm.nms.domain.comment.entity;
 
 import com.dsm.nms.domain.BaseTimeEntity;
 import com.dsm.nms.domain.notice.entity.Notice;
+import com.dsm.nms.domain.reply.entity.Reply;
 import com.dsm.nms.domain.student.entity.Student;
 import com.dsm.nms.domain.teacher.entity.Teacher;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,5 +33,8 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "teacher_id")
     @ManyToOne(fetch = FetchType.LAZY)
     private Teacher teacher;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
+    private List<Reply> replies;
 
 }
