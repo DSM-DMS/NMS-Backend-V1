3ea6bf9afa3dc4374a3300f749791909aebd190a
김범진 <sdpthf@naver.com>
2021-11-27 14:21:50 +0900

    ⚡ :: getAllNotices

--- src/main/java/com/dsm/nms/domain/notice/service/NoticeService.java
+++ src/main/java/com/dsm/nms/domain/notice/service/NoticeService.java
@@ -2,6 +2,7 @@ package com.dsm.nms.domain.notice.service;
 
 import com.dsm.nms.domain.notice.api.dto.request.ModifyNoticeRequest;
 import com.dsm.nms.domain.notice.api.dto.request.RegisterNoticeRequest;
+import com.dsm.nms.domain.notice.api.dto.response.NoticeResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface NoticeService {
     void registerNotice(RegisterNoticeRequest noticeRequest, List<MultipartFile> images);
     void modifyNotice(Integer noticeId, ModifyNoticeRequest noticeRequest, List<MultipartFile> images);
     void removeNotice(Integer noticeId);
+    NoticeResponse getAllNotices();
 }
--- src/main/java/com/dsm/nms/domain/notice/service/NoticeServiceImpl.java
+++ src/main/java/com/dsm/nms/domain/notice/service/NoticeServiceImpl.java
@@ -1,12 +1,18 @@
 package com.dsm.nms.domain.notice.service;
 
+import com.dsm.nms.domain.comment.entity.Comment;
+import com.dsm.nms.domain.comment.repository.CommentRepository;
+import com.dsm.nms.domain.image.entity.Image;
 import com.dsm.nms.domain.image.facade.ImageFacade;
-import com.dsm.nms.domain.notice.api.dto.ModifyNoticeRequest;
-import com.dsm.nms.domain.notice.api.dto.RegisterNoticeRequest;
+import com.dsm.nms.domain.notice.api.dto.request.ModifyNoticeRequest;
+import com.dsm.nms.domain.notice.api.dto.request.RegisterNoticeRequest;
+import com.dsm.nms.domain.notice.api.dto.response.NoticeResponse;
 import com.dsm.nms.domain.notice.entity.Notice;
 import com.dsm.nms.domain.notice.exception.NoticeNotFoundException;
 import com.dsm.nms.domain.notice.facade.NoticeFacade;
 import com.dsm.nms.domain.notice.repository.NoticeRepository;
+import com.dsm.nms.domain.reply.repository.ReplyRepository;
+import com.dsm.nms.domain.star.facade.StarFacade;
 import com.dsm.nms.domain.teacher.entity.Teacher;
 import com.dsm.nms.domain.teacher.facade.TeacherFacade;
 import lombok.RequiredArgsConstructor;
@@ -16,14 +22,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 @RequiredArgsConstructor
 @Service
 public class NoticeServiceImpl implements NoticeService {
 
+    private final StarFacade starFacade;
     private final ImageFacade imageFacade;
     private final NoticeFacade noticeFacade;
     private final TeacherFacade teacherFacade;
     private final NoticeRepository noticeRepository;
+    private final ReplyRepository replyRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     @Transactional
@@ -51,6 +62,73 @@ public class NoticeServiceImpl implements NoticeService {
         Notice notice = noticeFacade.getByNoticeId(noticeId);
         imageFacade.removeImages(notice);
         noticeRepository.delete(notice);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public NoticeResponse getAllNotices() {
+        Integer count = getCounts(noticeRepository.count());
+
+        List<NoticeResponse.notice> notices =  noticeRepository.findAll()
+                .stream()
+                .map(notice -> {
+                    return NoticeResponse.notice.builder()
+                            .noticeId(notice.getId())
+                            .title(notice.getTitle())
+                            .content(notice.getContent())
+                            .writer(NoticeResponse.writer.builder()
+                                    .name(notice.getTeacher().getName())
+                                    .profileUrl(notice.getTeacher().getProfileUrl())
+                                    .build())
+                            .createdDate(notice.getCreatedDate())
+                            .updatedDate(notice.getUpdatedDate())
+                            .images(notice.getImages().stream()
+                                    .map(Image::getImageUrl)
+                                    .collect(toList()))
+                            .isStar(starFacade.checkIsStar(notice))
+                            .commentCount(getCommentCount(notice))
+                            .comments(notice.getComments().stream()
+                                    .map(comment -> {
+                                        return NoticeResponse.comment.builder()
+                                                .id(comment.getId())
+                                                .writer(NoticeResponse.writer.builder()
+                                                        .name(comment.getStudent().getName())
+                                                        .profileUrl(comment.getStudent().getProfileUrl())
+                                                        .build())
+                                                .content(comment.getContent())
+                                                .createdDate(comment.getCreatedDate())
+                                                .replies(comment.getReplies().stream()
+                                                        .map(reply -> {
+                                                            return NoticeResponse.reply.builder()
+                                                                    .id(reply.getId())
+                                                                    .writer(NoticeResponse.writer.builder()
+                                                                            .name(reply.getTeacher().getName())
+                                                                            .profileUrl(reply.getTeacher().getProfileUrl())
+                                                                            .build())
+                                                                    .content(reply.getContent())
+                                                                    .createdDate(reply.getCreatedDate())
+                                                                    .build();
+                                                        })
+                                                        .collect(toList()))
+                                                .build();
+                                    })
+                                    .collect(toList()))
+                            .build();
+                })
+                .collect(toList());
+
+        return new NoticeResponse(count, notices);
+    }
+
+    private Integer getCounts(long count) {
+        return Math.toIntExact(count);
+    }
+
+    private Integer getCommentCount(Notice notice) {
+        return getCounts(commentRepository.countByNoticeId(notice.getId())) +
+                getCounts(replyRepository.countByCommentId(notice.getComments().stream()
+                        .map(Comment::getId)
+                        .collect(toList())));
     }
 
 }
