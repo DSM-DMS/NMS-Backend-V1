c09c9883158ba888b6f926a3f0330118ce6ae340
김범진 <sdpthf@naver.com>
2021-12-02 22:47:20 +0900

    ⚡ :: addReply

--- src/main/java/com/dsm/nms/domain/reply/service/ReplyServiceImpl.java
+++ src/main/java/com/dsm/nms/domain/reply/service/ReplyServiceImpl.java
@@ -1,8 +1,13 @@
 package com.dsm.nms.domain.reply.service;
 
 import com.dsm.nms.domain.reply.facade.ReplyFacade;
+import com.dsm.nms.domain.student.entity.Student;
+import com.dsm.nms.domain.student.facade.StudentFacade;
+import com.dsm.nms.domain.teacher.entity.Teacher;
 import com.dsm.nms.domain.teacher.facade.TeacherFacade;
+import com.dsm.nms.global.exception.InvalidRoleException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,11 +17,19 @@ public class ReplyServiceImpl implements ReplyService {
 
     private final ReplyFacade replyFacade;
     private final TeacherFacade teacherFacade;
+    private final StudentFacade studentFacade;
 
     @Override
     @Transactional
     public void addReply(Integer commentId, String content) {
-        replyFacade.createReply(commentId, content, teacherFacade.getCurrentTeacher());
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof Teacher) {
+            replyFacade.createReply(commentId, content, teacherFacade.getCurrentTeacher());
+        } else if (principal instanceof Student) {
+            replyFacade.createReply(commentId, content, studentFacade.getCurrentStudent());
+        } else throw InvalidRoleException.EXCEPTION;
+        
     }
 
 }
