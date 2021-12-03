ecb10bf35b015e09119c0fea3f33d6d391f91d14
이준서 <ljchan71@gmail.com>
2021-12-03 13:21:40 +0900

    ⚡ :: CommentService

--- src/main/java/com/dsm/nms/domain/comment/service/CommentService.java
+++ src/main/java/com/dsm/nms/domain/comment/service/CommentService.java
@@ -1,8 +1,6 @@
 package com.dsm.nms.domain.comment.service;
 
-import com.dsm.nms.domain.comment.api.dto.request.CommentRequest;
-
 public interface CommentService {
-    void comment(int noticeId, CommentRequest commentRequest);
-    void delComment(int commentId);
+    void addComment(int noticeId, String content);
+    void removeComment(int commentId);
 }
--- src/main/java/com/dsm/nms/domain/comment/service/CommentServiceImpl.java
+++ src/main/java/com/dsm/nms/domain/comment/service/CommentServiceImpl.java
@@ -3,36 +3,42 @@ package com.dsm.nms.domain.comment.service;
 import com.dsm.nms.domain.comment.api.dto.request.CommentRequest;
 import com.dsm.nms.domain.comment.entity.Comment;
 import com.dsm.nms.domain.comment.exception.CommentNotFoundException;
+import com.dsm.nms.domain.comment.facade.CommentFacade;
 import com.dsm.nms.domain.comment.repository.CommentRepository;
 import com.dsm.nms.domain.student.entity.Student;
 import com.dsm.nms.domain.student.facade.StudentFacade;
+import com.dsm.nms.domain.teacher.entity.Teacher;
+import com.dsm.nms.domain.teacher.facade.TeacherFacade;
+import com.dsm.nms.global.exception.InvalidRoleException;
+import com.dsm.nms.global.utils.auth.user.UserUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CommentServiceImpl implements CommentService{
 
-    private final CommentRepository commentRepository;
+    private final CommentFacade commentFacade;
+    private final TeacherFacade teacherFacade;
     private final StudentFacade studentFacade;
+    private final UserUtil userUtil;
 
     @Override
-    public void comment(int noticeId, CommentRequest commentRequest) {
+    public void addComment(int noticeId, String content) {
 
-        Student student = studentFacade.getCurrentStudent();
+        Object principal = userUtil.getPrincipal();
 
-        commentRepository.save(Comment.builder()
-                .commentRequest(commentRequest)
-                .name(student.getName())
-                .profileUrl(student.getProfileUrl())
-                .build());
+        if (principal instanceof Teacher) {
+            commentFacade.createComment(noticeId, content, teacherFacade.getCurrentTeacher());
+        } else if (principal instanceof Student) {
+            commentFacade.createComment(noticeId, content, studentFacade.getCurrentStudent());
+        } else throw InvalidRoleException.EXCEPTION;
+
     }
 
     @Override
-    public void delComment(int commentId) {
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(() -> CommentNotFoundException.EXCEPTION);
-
-        commentRepository.delete(comment);
+    public void removeComment(int commentId) {
+        commentFacade.removeComment(commentId);
     }
 }
