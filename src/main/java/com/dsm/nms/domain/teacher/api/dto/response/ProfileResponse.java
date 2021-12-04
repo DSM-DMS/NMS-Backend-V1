6bc4f464c42d33e5fa9588b9f3e843adb945b087
김범진 <sdpthf@naver.com>
2021-12-04 20:37:30 +0900

    📑 :: ProfileResponse

---
+++ src/main/java/com/dsm/nms/domain/teacher/api/dto/response/ProfileResponse.java
@@ -0,0 +1,29 @@
+package com.dsm.nms.domain.teacher.api.dto.response;
+
+import com.dsm.nms.domain.teacher.entity.Department;
+import com.dsm.nms.domain.teacher.entity.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProfileResponse {
+    private final String name;
+    private final String email;
+    private final String username;
+    private final Department department;
+    private final String phoneNumber;
+    private final String introduce;
+    private final String profileUrl;
+
+    public ProfileResponse(Teacher teacher) {
+        this.name = teacher.getName();
+        this.email = teacher.getEmail();
+        this.username = teacher.getUsername();
+        this.department = teacher.getDepartment();
+        this.phoneNumber = teacher.getPhoneNumber();
+        this.introduce = teacher.getIntroduction();
+        this.profileUrl = teacher.getProfileUrl();
+    }
+
+}
