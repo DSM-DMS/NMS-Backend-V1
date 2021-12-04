a5b34ef8f8849372bfbae7d744f827e94af9cb62
이준서 <ljchan71@gmail.com>
2021-12-04 18:39:08 +0900

    ⚡ :: RefreshTokenRepository

--- src/main/java/com/dsm/nms/domain/refreshtoken/repository/RefreshTokenRepository.java
+++ src/main/java/com/dsm/nms/domain/refreshtoken/repository/RefreshTokenRepository.java
@@ -4,6 +4,9 @@ import com.dsm.nms.domain.refreshtoken.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 }
