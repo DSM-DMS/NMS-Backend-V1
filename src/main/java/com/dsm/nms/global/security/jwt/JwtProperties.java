acdacd4c3f20605226a72b2885f355ca063b9b43
이준서 <ljchan71@gmail.com>
2021-12-04 18:40:32 +0900

    ⚡ :: secret키 인코딩

--- src/main/java/com/dsm/nms/global/security/jwt/JwtProperties.java
+++ src/main/java/com/dsm/nms/global/security/jwt/JwtProperties.java
@@ -1,12 +1,13 @@
 package com.dsm.nms.global.security.jwt;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 @Getter
-@AllArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
@@ -17,4 +18,11 @@ public class JwtProperties {
     private final String header;
     private final String prefix;
 
+    public JwtProperties(String secretKey, Long accessExp, Long refreshExp, String header, String prefix) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.accessExp = accessExp;
+        this.refreshExp = refreshExp;
+        this.header = header;
+        this.prefix = prefix;
+    }
 }
