6d05fd00009d9cd9abf5239fae199a84b857bb77
김범진 <sdpthf@naver.com>
2021-12-08 23:26:58 +0900

    ⚡ :: Enable scheduling

--- src/main/java/com/dsm/nms/NmsBackendV1Application.java
+++ src/main/java/com/dsm/nms/NmsBackendV1Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableBatchProcessing
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
