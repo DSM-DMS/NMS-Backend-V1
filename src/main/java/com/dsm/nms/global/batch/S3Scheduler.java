816b7f0b0aee5f23f9530f1812464e08b4a0fb1e
김범진 <sdpthf@naver.com>
2021-12-08 09:44:17 +0900

    ⚡ :: S3Scheduler

--- src/main/java/com/dsm/nms/global/batch/S3Scheduler.java
+++ src/main/java/com/dsm/nms/global/batch/S3Scheduler.java
@@ -1,14 +1,18 @@
 package com.dsm.nms.global.batch;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class S3Scheduler {
 
-    @Scheduled(cron = "0 0 0 1/1 * ? *")
-    public void removeFiles() {
+    private final CustomItemReader customItemReader;
 
+    @Scheduled(cron = "0 0 0 1/1 * ? *")
+    public void cleanFiles() {
+        customItemReader.jpaPagingItemReaderJob();
     }
 
 }
