f433b266862e96697355b3fd1b0f43b5c4138996
이준서 <ljchan71@gmail.com>
2021-12-12 23:00:24 +0900

    🐛 :: fetch.lazy 오류

--- src/main/java/com/dsm/nms/domain/star/repository/StarRepository.java
+++ src/main/java/com/dsm/nms/domain/star/repository/StarRepository.java
@@ -5,10 +5,11 @@ import com.dsm.nms.domain.star.entity.Star;
 import com.dsm.nms.domain.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StarRepository extends JpaRepository<Star, Integer> {
     Optional<Star> findByNoticeId(Integer noticeId);
     Optional<Star> findByNoticeAndStudent(Notice notice, Student student);
-    Integer countByNotice(Notice notice);
+    List<Star> findByStudent(Student student);
 }
